ba537ac6d10a5f6a74f57313d1d0ef6852db25c1
arfajsheru <arfatsheru74@gmail.com>
2025-01-14 11:25:00 +0530

    test files added

---
+++ DSA-Solution-And-Practice/src/in/arfajsheru/dsasolution/array/remover.java
@@ -0,0 +1,7 @@
+package in.arfajsheru.dsasolution.array;
+
+public class remover {
+    public static void main(String[] args) {
+        System.out.println("HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+    }
+}
