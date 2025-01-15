1a9a811d956e95f3c708281cee67083ab60dd695
arfajsheru <arfatsheru74@gmail.com>
2025-01-15 12:03:26 +0530

    14 and 58 String problem solved

---
+++ Leetcode-Easy-Problems/src/in/arfajsheru/string/58. LengthOfLastWord.java
@@ -0,0 +1,18 @@
+package in.arfajsheru.string;
+
+class LengthOfLastWord {
+   public static int lengthoflastword(String s){
+        s.trim();
+     for(int i = s.length() - 1; i >= 0; i--){
+         if(s.charAt(i) == ' '){
+	   return s.length() - (i + 1);
+       }
+   }
+	return s.length();
+  }
+
+    public static void main(String[] args) {
+        System.out.println(lengthoflastword("   I currently learning DSA in Java   "));
+
+    }
+}
