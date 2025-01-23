630464c262b2579911116e1a00598d2437514de0
arfajsheru <arfatsheru74@gmail.com>
2025-01-23 09:20:52 +0530

    Array 169 Problem solved using sorting

--- Leetcode-Easy-Problems/src/in/arfajsheru/array/283. MoveZeroes.java
+++ Leetcode-Easy-Problems/src/in/arfajsheru/array/283. MoveZeroes.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 class MoveZeroes {
     public static void main(String[] args) {
-        int[] arr = new int[] {0,1,0,3,12};
+        int[] arr = new int[] {1};
         moveZeroes(arr);
         System.out.println("OutPut is : " + Arrays.toString(arr));
     }
@@ -13,9 +13,11 @@ class MoveZeroes {
     public static void moveZeroes(int[] arr){
         int prev = 0;
         for(int i = 0; i < arr.length; i++){
-            if(arr[i] != 0){
-                arr[prev++] = arr[i];
-                arr[i] = 0;
+            if(arr[i] != 0){ // 1 != 0 T
+                int temp = arr[i]; // temp = 1
+                arr[i] = arr[prev]; // 0Index = 1
+                arr[prev] = temp; // 0Index = 1
+                prev++;// prev++
             }
         }
     }
