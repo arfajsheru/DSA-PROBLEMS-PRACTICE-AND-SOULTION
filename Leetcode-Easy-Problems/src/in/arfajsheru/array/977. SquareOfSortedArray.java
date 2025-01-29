cda21b15fe016ec9724c5418091cce1a4cb640ac
arfajsheru <arfatsheru74@gmail.com>
2025-01-29 08:51:55 +0530

    problem 977 added

---
+++ Leetcode-Easy-Problems/src/in/arfajsheru/array/977. SquareOfSortedArray.java
@@ -0,0 +1,29 @@
+package in.arfajsheru.array;
+
+import java.util.Arrays;
+
+class SquareOfSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {-4,-1,0,3,10};
+        System.out.println(Arrays.toString(solution(nums)));
+    }
+
+    public static int[] solution(int[] nums){
+        int beg = 0, end = nums.length - 1;
+        int[] result = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++){
+            int sqr1 = (int) Math.pow(nums[beg], 2);
+            int sqr2 = (int) Math.pow(nums[end], 2);
+
+            if(sqr1 >= sqr2){
+                result[nums.length - 1 - i] = sqr1;
+                beg++;
+            }else{
+                result[nums.length - 1 - i] = sqr2;
+                end--;
+            }
+        }
+        return result;
+    }
+}
