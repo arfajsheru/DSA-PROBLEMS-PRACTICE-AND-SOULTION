754fad463d2694093f056e11387ce2a3af53d8c7
arfajsheru <arfatsheru74@gmail.com>
2025-02-03 09:41:00 +0530

    Problem 744 FindSmallestLetterGreaterThanTarget
    lLetter added

---
+++ Leetcode-Easy-Problems/src/in/arfajsheru/array/ThirdMaximumNumber.java
@@ -0,0 +1,4 @@
+package in.arfajsheru.array;
+
+public class ThirdMaximumNumber {
+}
--- Leetcode-Easy-Problems/src/in/arfajsheru/bitmanipulation/476. NumberComplement.java
+++ Leetcode-Easy-Problems/src/in/arfajsheru/bitmanipulation/476. NumberComplement.java
@@ -8,12 +8,14 @@ class NumberComplement {
     public static int solution(int num){
         int bitlength = Integer.toBinaryString(num).length(); // 4
 
-        int mask = (1 << bitlength) - 1; //( 0001 << 4 ) = 0000 -1 = 1111 = 15
-        System.out.println("mask = " + Integer.toBinaryString(mask));
-        System.out.println("num = " + Integer.toBinaryString(num));
+        int mask = (1 << bitlength) - 1; //( 1 << 3 (1000) ) = 8 - 1 = 111 = 7
+        System.out.println("mask in binary = " + Integer.toBinaryString(mask));
+        System.out.println("num in binary = " + Integer.toBinaryString(num));
 
-        int result = num ^ mask;
-        System.out.println("result = " + Integer.toBinaryString(result));
+        int result = num ^ mask; // 5 ^ 15 = 101
+                                           //111
+                                           //010
+        System.out.println("result in binary= " + Integer.toBinaryString(result));
 
         return result;
     }
